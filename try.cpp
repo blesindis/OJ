647047ef2f1d1de5691eaf7514188e8d826df8b3
blesindis <71581447+blesindis@users.noreply.github.com>
2022-01-24 14:47:37 +0800

    2022-1-24

--- try.cpp
+++ try.cpp
@@ -6,19 +6,11 @@
 #include <math.h>
 using namespace std;
 
-typedef struct aFree{
-    void *next;
-}Free;
 
 int main(){
     
-    double a = 0.199999999999999;
-    double b = 0.5;
-    for (int i = 0; i < 15; i++){
-        b = b * 10;
-    }
-
-    printf("%lf", b);
+    int i = 3;
+    printf("%d%d", i, !i);
 
     return 0;
 }
